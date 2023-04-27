5d33d9e488c80d29b53d0ed6e9e88e901293a103
KamilGmitrzuk <123082737+KamillGG@users.noreply.github.com>
2023-04-27 14:54:52 +0200

    wow

---
+++ app.js
@@ -0,0 +1,33 @@
+var map = L.map('map',{dragging:false}).setView([52.186858,21.572491], 7);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+var budowlana = L.marker([52.186858,21.572491]).addTo(map)
+    .bindPopup('Szkola')
+    .openPopup();
+
+map.on('click', function(e){
+    console.log(e.latlng)
+    marker(e.latlng)
+    
+    //getData(e.latlng)
+});
+// async function getData(latlng){
+//     const lng = latlng.lng
+//     const lat = latlng.lat
+//     console.log(lng)
+//     const url = `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${lat}&lon=${lng}`
+//     const data = await fetch(url).then(response => response.json())
+//     console.log(data)
+// }
+function marker(latlng){
+    var from = budowlana.getLatLng()
+    var znacznik = L.marker([latlng.lat,latlng.lng]).addTo(map)
+    var to = znacznik.getLatLng()
+    var odleglosc = from.distanceTo(to).toFixed(0)/1000 + "km"
+    znacznik.bindPopup(odleglosc).openPopup();
+    console.log()
+    
+}
